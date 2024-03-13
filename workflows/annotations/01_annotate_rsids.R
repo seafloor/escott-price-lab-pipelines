b62b9cdb122378f164c31163241508afbfdab820
seafloor <mattsmith418@gmail.com>
2024-03-13 11:56:43 +0000

    updated rsids annotations example to stable version

--- workflows/annotations/01_annotate_rsids.R
+++ workflows/annotations/01_annotate_rsids.R
@@ -1,17 +1,22 @@
 # load packages with box
 box::use(eplp = escottpricelabpipelines[rsids_to_functional_genes],
-         rdr = readr[read_csv, write_csv])
+         rdr = readr[read_csv, write_csv],
+         magrittr[`%>%`])
 
-rsids <- rdr$read_csv("~/Desktop/All_bellenguez_hits_80SNPs.txt",
+bellenguez_snps <- system.file("extdata", "annotations",
+                               "all_bellenguez_hits_common.txt",
+                               package = "escottpricelabpipelines")
+rsids <- rdr$read_csv(bellenguez_snps,
                       col_names = c("rsid"))
 
-annotations <- eplp$rsids_to_functional_genes(rsids[['rsid']])
+output <- eplp$rsids_to_functional_genes(rsids[['rsid']][1:3])
 
-rdr$write_csv(annotations, "~/Desktop/all_bellenguez_hits_annotations.csv")
+annotations <- output[[1]]
+missing_rsids <- output[[2]]
 
-rsids <- rdr$read_csv("~/Desktop/29_GWS_SNP_not_input_data.txt",
-                      col_names = c("rsid"))
+print(paste("--> Missing", length(missing_rsids),
+            "rsids in functional annotations"))
+print(missing_rsids)
 
-annotations <- eplp$rsids_to_functional_genes(rsids[['rsid']])
-
-rdr$write_csv(annotations, "~/Desktop/remaining_bellenguez_hits_annotations.csv")
+print("--> Functional annotations for the following rsids:")
+print(annotations)
